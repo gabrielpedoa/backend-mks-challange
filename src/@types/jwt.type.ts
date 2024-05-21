97a12f3200981b5a566eb69564ded715fdfff796
GABRIEL ANTUNES <gabrielpedoa@gmail.com>
2024-05-21 12:22:11 -0300

    jwt config

---
+++ src/@types/jwt.type.ts
@@ -0,0 +1,5 @@
+export type IJwtPayload = {
+    username: string;
+    userId: string;
+  };
+  
---
+++ src/helpers/jwt.helper.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { config } from 'dotenv';
+import { IJwtPayload } from 'src/@types/jwt.type';
+
+config();
+
+const SECRET = process.env.JWT_SECRET;
+
+@Injectable()
+export class JwtHelper {
+  private readonly jwt = new JwtService({
+    secret: SECRET,
+  });
+
+  async genToken(data: IJwtPayload): Promise<string> {
+    const token = await this.jwt.signAsync(JSON.stringify(data));
+    return token;
+  }
+
+  async verify(token: string): Promise<IJwtPayload> {
+    const payload = await this.jwt.verifyAsync(token);
+    return payload as IJwtPayload;
+  }
+}
